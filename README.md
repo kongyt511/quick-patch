fadd4783c9490d3e57d2e67c63d50c8c3b49d1d9
kongyatong <kongyatong@magictavern.com>
2022-09-20 18:15:42 +0800

    ios support

--- README.md
+++ README.md
@@ -12,8 +12,10 @@ adb
 ## Extension Settings
 This extension contributes the following settings:
 
-* `quick_patch.adb_path`: adb工具路径(默认是"adb"，需要在系统环境变量PATH中设置，也可以是绝对路径)
-* `quick_patch.dest_path`: 补丁推送的目的路径(默认是"/storage/emulated/0/Android/data/xxxxxx/files", 请替换成你自己的目录)
+* `quick_patch.adb_path`: [Android独有] adb工具路径(默认是"adb"，需要在系统环境变量PATH中设置，也可以是绝对路径)
+* `quick_patch.ifuse_path`: [IOS独有] ifuse工具路径(默认是"ifuse"，需要在系统环境变量PATH中设置，也可以是绝对路径)
+* `quick_patch.dest_path_android`: [Android独有]补丁推送的目的路径(默认是"/storage/emulated/0/Android/data/xxxxxx/files", 请替换成你自己的目录)
+* `quick_patch.bundle_id`: [IOS独有] App Bundle Id
 * `quick_patch.patch_dir`: 补丁根目录(默认是"debug")
 * `quick_patch.scripts_dir`: 脚本根目录(默认是"Scripts")
 
--- src/extension.ts
+++ src/extension.ts
@@ -27,10 +27,10 @@ class CommandRunner {
 
 const commandRunner = new CommandRunner();
 
-async function quickPatchSend(context: vscode.ExtensionContext, uri: vscode.Uri) {
+async function quickPatchSendAndroid(context: vscode.ExtensionContext, uri: vscode.Uri) {
 	const ext = vscode.workspace.getConfiguration('quick_patch')
 	const adbPath = ext.get<string>('adb_path') as string
-	const pushPath = ext.get<string>('dest_path') as string
+	const pushPath = ext.get<string>('dest_path_android') as string
 	const patchDir = ext.get<string>('patch_dir') as string
 	const scriptsDir = ext.get<string>('scripts_dir') as string
 
@@ -54,10 +54,10 @@ async function quickPatchSend(context: vscode.ExtensionContext, uri: vscode.Uri)
 	}
 }
 
-async function quickPatchClear(context: vscode.ExtensionContext) {
+async function quickPatchClearAndroid(context: vscode.ExtensionContext) {
 	const ext = vscode.workspace.getConfiguration('quick_patch')
 	const adbPath = ext.get<string>('adb_path') as string
-	const pushPath = ext.get<string>('dest_path') as string
+	const pushPath = ext.get<string>('dest_path_android') as string
 	const patchDir = ext.get<string>('patch_dir') as string
 
 	const tmpPath = context.storageUri?.fsPath as string
@@ -70,17 +70,88 @@ async function quickPatchClear(context: vscode.ExtensionContext) {
 	vscode.window.showInformationMessage('清理Patch成功')
 }
 
+async function quickPatchSendIOS(context: vscode.ExtensionContext, uri: vscode.Uri) {
+	const ext = vscode.workspace.getConfiguration('quick_patch')
+	const ifusePath = ext.get<string>('ifuse_path') as string
+	const bundleId = ext.get<string>('bundle_id') as string
+	const tmpPath = context.storageUri?.fsPath as string
+	const pushPath = tmpPath + '/' + bundleId
+	const patchDir = ext.get<string>('patch_dir') as string
+	const scriptsDir = ext.get<string>('scripts_dir') as string
+	
+	if (!fs.existsSync(pushPath)) {
+		fs.mkdirSync(pushPath, { recursive: true })
+	}
+	var cmd = ifusePath + ' --documents ' + bundleId + ' \"' + pushPath + '\"';
+	commandRunner.runTerminalCommand(cmd)
+
+	const filePath = uri.fsPath
+	const fileDir = path.parse(filePath).dir
+	const tmpPatchPath = path.join(tmpPath, patchDir)
+	const startPos = fileDir.indexOf(scriptsDir)
+	if (startPos >= 0) {
+		const destPath = fileDir.substring(startPos).replace(scriptsDir, tmpPatchPath)
+		const destFile = path.join(destPath, filePath.split(path.sep).slice(-1)[0])
+		if (!fs.existsSync(destPath)) {
+			fs.mkdirSync(destPath, { recursive: true })
+		}
+		fs.copyFileSync(filePath, destFile)
+		var destPathDir = path.join(pushPath, patchDir)
+		commandRunner.runTerminalCommand('rm -rf  \"' + destPathDir + '\"')
+		commandRunner.runTerminalCommand('cp -r \"' + tmpPatchPath + '\" \"' + pushPath + '\"')
+		commandRunner.runTerminalCommand('umount -f ' + ' \"' + pushPath + '\"')
+		vscode.window.showInformationMessage('发送文件到设备:', uri.fsPath)
+	} else {
+		vscode.window.showErrorMessage('文件不在脚本文件夹中:', uri.fsPath)
+	}
+}
+
+async function quickPatchClearIOS(context: vscode.ExtensionContext) {
+	const ext = vscode.workspace.getConfiguration('quick_patch')
+	const ifusePath = ext.get<string>('ifuse_path') as string
+	const bundleId = ext.get<string>('bundle_id') as string
+	const tmpPath = context.storageUri?.fsPath as string
+	const pushPath = tmpPath + '/' + bundleId
+	
+	if (!fs.existsSync(pushPath)) {
+		fs.mkdirSync(pushPath, { recursive: true })
+	}
+	var cmd = ifusePath + ' --documents ' + bundleId + ' \"' + pushPath + '\"'
+	commandRunner.runTerminalCommand(cmd)
+
+	const patchDir = ext.get<string>('patch_dir') as string
+	const tmpPatchPath = path.join(tmpPath, patchDir)
+	if (fs.existsSync(tmpPatchPath)) {
+		fs.rmdirSync(tmpPatchPath, { recursive: true })
+	}
+	var destPathDir = path.join(pushPath, patchDir)
+	commandRunner.runTerminalCommand('rm -rf  \"' + destPathDir + '\"')
+	commandRunner.runTerminalCommand('umount -f ' + ' \"' + pushPath + '\"')
+	vscode.window.showInformationMessage('清理Patch成功')
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	let quickPatchSendCommand = vscode.commands.registerCommand('quick_patch_send', (uri: vscode.Uri) => {
-		quickPatchSend(context, uri)
+	let quickPatchSendAndroidCommand = vscode.commands.registerCommand('quick_patch_send_android', (uri: vscode.Uri) => {
+		quickPatchSendAndroid(context, uri)
 	});
 
-	let quickPatchClearCommand = vscode.commands.registerCommand('quick_patch_clear', () => {
-		quickPatchClear(context)
+	let quickPatchClearAndroidCommand = vscode.commands.registerCommand('quick_patch_clear_android', () => {
+		quickPatchClearAndroid(context)
+	});
+	
+	let quickPatchSendIOSCommand = vscode.commands.registerCommand('quick_patch_send_ios', (uri: vscode.Uri) => {
+		quickPatchSendIOS(context, uri)
 	});
 
-	context.subscriptions.push(quickPatchSendCommand);
-	context.subscriptions.push(quickPatchClearCommand);
+	let quickPatchClearIOSCommand = vscode.commands.registerCommand('quick_patch_clear_ios', () => {
+		quickPatchClearIOS(context)
+	});
+
+	context.subscriptions.push(quickPatchSendAndroidCommand);
+	context.subscriptions.push(quickPatchClearAndroidCommand);
+	
+	context.subscriptions.push(quickPatchSendIOSCommand);
+	context.subscriptions.push(quickPatchClearIOSCommand);
 }
 
 export function deactivate() {}
